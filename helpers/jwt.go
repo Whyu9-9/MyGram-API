739d4fd98792d236d103d42558bcec8da2eb92b7
Wahyu <wahyu@Wahyus-MacBook-Pro.local>
2022-10-17 20:03:06 +0800

    add helpers for jwt, bcrypt, and headers

---
+++ helpers/jwt.go
@@ -0,0 +1,60 @@
+package helpers
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	secret = "mYgr4mAp1"
+)
+
+func GenerateToken(id uint, email string) string {
+	claims := jwt.MapClaims{
+		"id":    id,
+		"email": email,
+	}
+
+	parseToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := parseToken.SignedString([]byte(secret))
+
+	if err != nil {
+		panic("Error while signing token")
+	}
+
+	return signedToken
+}
+
+func VerifyToken(c *gin.Context) (interface{}, error) {
+	errResponse := errors.New("Wrong token")
+	headerToken := c.Request.Header.Get("Authorization")
+	bearer := strings.HasPrefix(headerToken, "Bearer")
+
+	if !bearer {
+		return nil, errResponse
+	}
+
+	jwtString := strings.Split(headerToken, " ")[1]
+
+	token, err := jwt.Parse(jwtString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errResponse
+		}
+
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, errResponse
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok && !token.Valid {
+		return nil, errResponse
+	}
+
+	return token.Claims, nil
+
+}
